44a2f471330adca0c812cbb9f66983d8d34b779d
Ditler-M <154798663+Ditler-M@users.noreply.github.com>
2023-12-25 13:12:41 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,143 @@
+let tg = window.Telegram.WebApp;
+tg.expand();
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
+let item = "";
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+btn1.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по ");
+        item = "1";
+        tg.MainButton.show();
+    }
+});
+
+btn2.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по ");
+        item = "2";
+        tg.MainButton.show();
+    }
+});
+
+btn3.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по ");
+        item = "3";
+        tg.MainButton.show();
+    }
+});
+
+btn4.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по ");
+        item = "4";
+        tg.MainButton.show();
+    }
+});
+
+btn5.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по ");
+        item = "5";
+        tg.MainButton.show();
+    }
+});
+
+btn6.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по ");
+        item = "6";
+        tg.MainButton.show();
+    }
+});
+
+btn7.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по ");
+        item = "7";
+        tg.MainButton.show();
+    }
+});
+
+btn8.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по ");
+        item = "8";
+        tg.MainButton.show();
+    }
+});
+
+btn9.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по ");
+        item = "9";
+        tg.MainButton.show();
+    }
+});
+
+btn10.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по ");
+        item = "10";
+        tg.MainButton.show();
+    }
+});
+
+btn11.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по ");
+        item = "11";
+        tg.MainButton.show();
+    }
+});
+
+btn12.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по ");
+        item = "12";
+        tg.MainButton.show();
+    }
+});
+
+
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+    tg.sendData(item);
+});
